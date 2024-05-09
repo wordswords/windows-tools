03dc45844fe90c31f32028c7eccad837fcd7ff51
David Craddock <contact@davidcraddock.net>
2024-05-09 21:13:50 +0100

    Fix for weird character thing in windows path

--- windows-terminal-emulators-config/install-alacritty-windows.sh
+++ windows-terminal-emulators-config/install-alacritty-windows.sh
@@ -3,8 +3,9 @@
 set -x
 set -e
 
-sudo mkdir -p "/mnt/c/Program\ Files/alacritty"
-sudo wget https://github.com/alacritty/alacritty/releases/download/v0.13.2/Alacritty-v0.13.2-portable.exe -O "/mnt/c/Program\ Files/alacritty/Alacritty-v0.13.2-portable.exe"
+sudo rm -rf "/mnt/C/Program Files/alacritty"
+sudo mkdir -p "/mnt/C/Program Files/alacritty"
+sudo wget https://github.com/alacritty/alacritty/releases/download/v0.13.2/Alacritty-v0.13.2-portable.exe -O "/mnt/c/Program Files/alacritty/Alacritty-v0.13.2-portable.exe"
 mkdir -p /mnt/c/Users/conta/AppData/Roaming/alacritty
 cp ~/.dotfiles/windows-tools/windows-terminal-emulators-config/windows-alacritty.toml /mnt/c/Users/conta/AppData/Roaming/alacritty/alacritty.toml
 echo "Now you can run Alacritty by running C:\Program Files\alacritty\Alacritty-v0.13.2-portable.exe from within windows and it will launch WSL"
