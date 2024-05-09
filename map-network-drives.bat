3126ac9d8a7ddf931152253acfc4da3ae072fcad
David Craddock <contact@davidcraddock.net>
2024-05-09 13:26:10 +0100

    Windows tools initial commit

---
+++ map-network-drives.bat
@@ -0,0 +1,13 @@
+net use I: \\hq.local\incominggames
+net use J: \\hq.local\incomingmusic
+net use K: \\hq.local\incomingtvvideo
+net use M: \\hq.local\music
+net use N: \\hq.local\incomingebooks
+net use O: \\hq.local\incomingcomics
+net use X: \\hq.local\games
+net use Z: \\hq.local\incomingscratch
+
+
+
+
+
