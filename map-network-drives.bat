d2b9f303d95c370afb328f29a6885fa027a9f547
David Craddock <contact@davidcraddock.net>
2025-01-01 20:14:52 +0000

    Better version of map-network-drives

--- map-network-drives.bat
+++ map-network-drives.bat
@@ -1,9 +1,20 @@
-net use J: \\192.168.1.50\incominggames
-net use K: \\192.168.1.50\incomingmusic
-net use L: \\192.168.1.50\incomingtvvideo
-net use M: \\192.168.1.50\music
-net use N: \\192.168.1.50\incomingebooks
-net use O: \\192.168.1.50\incomingcomics
-net use X: \\192.168.1.50\games
-net use Z: \\192.168.1.50\incomingscratch
+net use J: /delete
+net use K: /delete
+net use L: /delete
+net use M: /delete
+net use N: /delete
+net use O: /delete
+net use X: /delete
+net use Z: /delete
+
+set h=hq.local
+
+net use J: \\%h%\incominggames
+net use K: \\%h%\incomingmusic
+net use L: \\%h%\incomingtvvideo
+net use M: \\%h%\music
+net use N: \\%h%\incomingebooks
+net use O: \\%h%\incomingcomics
+net use X: \\%h%\games
+net use Z: \\%h%\incomingscratch
 
