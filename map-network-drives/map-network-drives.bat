a1692524a9fdd501ebb57bfd1699c9e5e401cc2a
David Craddock <contact@davidcraddock.net>
2025-05-21 05:51:17 +0100

    Cleaned up repo

---
+++ map-network-drives/map-network-drives.bat
@@ -0,0 +1,21 @@
+net use J: /delete
+net use K: /delete
+net use L: /delete
+net use M: /delete
+net use N: /delete
+net use O: /delete
+net use X: /delete
+net use Y: /delete
+net use Z: /delete
+
+set h=hq.local
+
+net use J: \\%h%\incominggames
+net use K: \\%h%\incomingmusic
+net use L: \\%h%\incomingtvvideo
+net use N: \\%h%\incomingebooks
+net use O: \\%h%\incomingcomics
+net use X: \\%h%\games
+net use Y: \\%h%\music
+net use Z: \\%h%\incomingscratch
+
