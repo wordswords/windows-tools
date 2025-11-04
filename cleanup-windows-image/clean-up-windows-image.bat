3013e55757637835c23bf7b8d7387a8cece473e2
David Craddock <contact@davidcraddock.net>
2025-11-04 19:23:49 +0000

    Updated with a pause at the end so doesn't autoclose

--- cleanup-windows-image/clean-up-windows-image.bat
+++ cleanup-windows-image/clean-up-windows-image.bat
@@ -2,5 +2,7 @@ DISM.exe /Online /Cleanup-image /Scanhealth
 DISM.exe /Online /Cleanup-image /Restorehealth
 DISM.exe /Online /Cleanup-image /startcomponentcleanup
 sfc /scannow
-
+@echo off
+echo Batch file finished. Please press any key to Exit...
+pause
 
