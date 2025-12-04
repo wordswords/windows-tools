38609aa046ed3ca7a1341843c248cbe58c72a009
David Craddock <contact@davidcraddock.net>
2025-12-04 07:38:19 +0000

    New drive layout

--- map-network-drives/map-network-drives.bat
+++ map-network-drives/map-network-drives.bat
@@ -1,21 +1,23 @@
+net use G: /delete
 net use J: /delete
 net use K: /delete
 net use L: /delete
 net use M: /delete
 net use N: /delete
 net use O: /delete
+net use T: /delete
 net use X: /delete
 net use Y: /delete
 net use Z: /delete
 
-set h=hq.local
+set h=192.168.1.200
 
+net use G: \\%h%\games
 net use J: \\%h%\incominggames
 net use K: \\%h%\incomingmusic
-net use L: \\%h%\incomingtvvideo
+net use M: \\%h%\music
 net use N: \\%h%\incomingebooks
+net use T: \\%h%\incomingtvvideo
 net use O: \\%h%\incomingcomics
-net use X: \\%h%\games
-net use Y: \\%h%\music
 net use Z: \\%h%\incomingscratch
 
