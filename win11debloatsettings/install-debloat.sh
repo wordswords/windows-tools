4c95a792497ee9cfcd8e99b7ed2dee1ca1f70483
David Craddock <contact@davidcraddock.net>
2026-03-06 03:59:24 +0000

    Fixed?

--- win11debloatsettings/install-debloat.sh
+++ win11debloatsettings/install-debloat.sh
@@ -4,13 +4,13 @@ set -e
 set -x
 
 mkdir -p /tmp/Win11Debloat
-rm -rf /tmp/Win11Debloat/*
+rm -rf /tmp/Win11Debloat
 git clone git@github.com:Raphire/Win11Debloat.git /tmp/Win11Debloat
 sudo rm -rf /mnt/c/Users/conta/My\ Documents/Win11Debloat
-sudo mv /tmp/Win11Debloat/ "/mnt/c/Users/conta/My\ Documents/Win11Debloat"
-sudo cp ./DefaultSettings.json "/mnt/c/Users/conta/My\ Documents/Win11Debloat"
-sudo mkdir -p "/mnt/c/Users/conta/AppData/Roaming/Microsoft/Windows/Start\ Menu/Programs/Startup/"
-sudo cp ./documents-dir-ps1-to-run-debloat.ps1 "/mnt/c/Users/conta/My\ Documents/Win11Debloat"
-sudo cp ./startup-dir-bat-to-run-ps1-file.bat "/mnt/c/Users/conta/AppData/Roaming/Microsoft/Windows/Start\ Menu/Programs/Startup/"
+sudo mv /tmp/Win11Debloat/ '/mnt/c/Users/conta/My Documents/Win11Debloat'
+sudo cp ./DefaultSettings.json '/mnt/c/Users/conta/My Documents/Win11Debloat'
+sudo mkdir -p '/mnt/c/Users/conta/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/'
+sudo cp ./documents-dir-ps1-to-run-debloat.ps1 '/mnt/c/Users/conta/My Documents/Win11Debloat'
+sudo cp ./startup-dir-bat-to-run-ps1-file.bat '/mnt/c/Users/conta/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/'
 
 
