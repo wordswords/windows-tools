f9942c8e83bce1156e141a98ff6b1404573fb7bf
David Craddock <contact@davidcraddock.net>
2026-03-06 04:08:58 +0000

    Hopefully fixed for good

--- win11debloatsettings/documents-dir-ps1-to-run-debloat.ps1
+++ win11debloatsettings/documents-dir-ps1-to-run-debloat.ps1
@@ -1,2 +1,2 @@
 Set-ExecutionPolicy Unrestricted -Scope Process -Force
-Start-Process powershell.exe -PassThru -ArgumentList "-executionpolicy bypass -WindowStyle Hidden -File C:\Users\conta\My Documents\Win11Debloat.ps1 -Silent -RunDefaults" -Verb RunAs
+Start-Process powershell.exe -PassThru -ArgumentList "-executionpolicy bypass -WindowStyle Hidden -File C:\Users\conta\Documents\Win11Debloat\Win11Debloat.ps1 -Silent -RunDefaults" -Verb RunAs
