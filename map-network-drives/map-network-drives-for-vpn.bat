69b6849922b3fd7d0629303316d86c1ba1d0bfbb
David Craddock <contact@davidcraddock.net>
2026-05-16 05:57:27 +0100

    Added vpn script

---
+++ map-network-drives/map-network-drives-for-vpn.bat
@@ -0,0 +1,25 @@
+net use B: /delete
+net use G: /delete
+net use F: /delete
+net use J: /delete
+net use K: /delete
+net use L: /delete
+net use M: /delete
+net use N: /delete
+net use O: /delete
+net use T: /delete
+net use X: /delete
+net use Y: /delete
+net use Z: /delete
+
+set h=192.168.100.200
+
+net use G: \\%h%\games
+net use J: \\%h%\incominggames
+net use K: \\%h%\incomingmusic
+net use M: \\%h%\music
+net use N: \\%h%\incomingebooks
+net use T: \\%h%\incomingtvvideo
+net use O: \\%h%\incomingcomics
+net use Z: \\%h%\incomingscratch
+
